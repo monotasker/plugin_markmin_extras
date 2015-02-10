5e4e19c82d7e51ea5251d1eff0ec4e3b606108c1
Ian W. Scott <scottianw@gmail.com>
2015-02-10 15:47:03 -0500

    minor code cleanup, no substantive change to functions

--- modules/plugin_markmin_extras.py
+++ modules/plugin_markmin_extras.py
@@ -24,7 +24,7 @@ This plugin assumes the accompanying controller function (also in the
 packaged plugin) to provide download access to the files.
 
 """
-from pprint import pprint
+# from pprint import pprint
 from gluon import current, URL
 db = current.db
 
@@ -44,17 +44,17 @@ imgrows = db(db.blog_images.id > 0).select().as_list()
 imgrows2 = db(db.woh_images.id > 0).select().as_list()
 audrows = db(db.woh_audio.id > 0).select().as_list()
 docrows = db(db.docs.id > 0).select().as_list()
-print 'in markmin_extras'
-pprint(docrows)
-pprint(docrows[0])
 
-maketag = lambda(t, txt): URL('plugin_markmin_extras', 'download',
-                              td[t]['table'](td[t]['titlefield'] == txt
-                                             )[td[t]['filefield']]
-                              ).xml()
 
-mm_extras = dict(img_by_title=lambda text: maketag('image', text),
-                 audio_by_title=lambda text: maketag('audio', text),
+def maketag(td, t, txt):
+    """docstring for ma"""
+    url = URL('plugin_markmin_extras', 'download',
+              td[t]['table'](td[t]['titlefield'] == txt)[td[t]['filefield']]
+              ).xml()
+    return url
+
+mm_extras = dict(img_by_title=lambda text: maketag(td, 'image', text),
+                 audio_by_title=lambda text: maketag(td, 'audio', text),
                  doc_by_title=lambda text: '<a href={}>{}</a> '
                     ''.format(URL('blog/plugin_markmin_extras', 'download',
                                   [d['docfile'] for d in docrows
